7a19d1b7ba4b8641003d76bad652702084a0e0f7
Anthony Mattas <anthony@mattas.net>
2022-12-14 07:11:26 -0500

    Update PowerBiManager.cs
    Added comments for better understanding

--- AzureFunction/Services/PowerBiManager.cs
+++ AzureFunction/Services/PowerBiManager.cs
@@ -11,28 +11,33 @@ using System.Threading.Tasks;
 namespace GetEmbedToken.Services {
 
   class PowerBiManager {
-
     static string accessToken = TokenManager.GetAadAccessToken();
-
     private const string urlPowerBiServiceApiRoot = "https://api.powerbi.com/";
     static TokenCredentials tokenCredentials = new TokenCredentials(accessToken, "Bearer");
     static PowerBIClient pbiClient = new PowerBIClient(new Uri(urlPowerBiServiceApiRoot), tokenCredentials);
 
+    /* Method to get the EmbedToken from the Power BI service. The Embed Token an authentication token
+        used to grant access to a set of reports and datasets when deploying a report in App Owns Data mode. 
+        this method could be updated to use a collection of reports instead of a single report like we do with
+        roles however that adds some complexity when mapping roles. To simplify this example we are only working 
+        with single reports. */
     public static async Task<string> GetEmbedToken(Guid WorkspaceId, Guid ReportId, string username, string roles) {
-
+      //Get report metadata from the Power BI service using the service principal.
       var report = await pbiClient.Reports.GetReportInGroupAsync(WorkspaceId,ReportId);
-
+      //Create list of associated datasets to request access to with the embed token.
       var datasetRequests = new List<GenerateTokenRequestV2Dataset>();
       datasetRequests.Add(new GenerateTokenRequestV2Dataset(report.DatasetId));
-
+      //Create list of reports to request access to with the Embed Token
       var reportRequests = new List<GenerateTokenRequestV2Report>();
       reportRequests.Add(new GenerateTokenRequestV2Report(report.Id, allowEdit: false));
-
+      //Create list of workspaces to request access to with the Embed Token.
       var workspaceRequests = new List<GenerateTokenRequestV2TargetWorkspace>();
       workspaceRequests.Add(new GenerateTokenRequestV2TargetWorkspace(WorkspaceId));
-
+      //Create list of effective identities to associate the Embed Token with.
       var effectiveIdentities = new List<EffectiveIdentity>();
       var effectiveIdentity = new EffectiveIdentity(username: username, datasets: new List<string>(){report.DatasetId.ToString()});
+      /* Create list of roles to request access to with the Embed token. These roles must exist in the report 
+          and the report must have at least one role for Effective Identity to work */
       if (roles != null) {
         var effectiveIdentitiiesRoles = new List<string>();
           foreach(var r in roles.Split(",")) {
@@ -41,7 +46,7 @@ namespace GetEmbedToken.Services {
         effectiveIdentity.Roles = effectiveIdentitiiesRoles;
       }
       effectiveIdentities.Add(effectiveIdentity);
-
+      //Create the actual token requests with the data generated above
       GenerateTokenRequestV2 tokenRequest =
         new GenerateTokenRequestV2 {
           Datasets = datasetRequests,
@@ -49,12 +54,13 @@ namespace GetEmbedToken.Services {
           Reports = reportRequests,
           TargetWorkspaces = workspaceRequests
         };
-        
+        /* Set the timeout for the Embed Token. After this period of time the function needs to be called again for a 
+            user to continue accessing the reports */ 
         tokenRequest.LifetimeInMinutes = 10;
 
       // call to Power BI Service API and pass GenerateTokenRequest object to generate embed token
-        var EmbedTokenResult = await pbiClient.EmbedToken.GenerateTokenAsync(tokenRequest);
-        return EmbedTokenResult.Token;
+      var EmbedTokenResult = await pbiClient.EmbedToken.GenerateTokenAsync(tokenRequest);
+      return EmbedTokenResult.Token;
     }
   }
 }
